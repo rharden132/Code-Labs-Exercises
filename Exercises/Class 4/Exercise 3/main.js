4f81a68450e6df24f2a0f4de4c91c59357b6f0d6
rharden132 <hardenx10@gmail.com>
2022-08-26 18:03:43 -0500

    new

--- Exercises/Class 4/Exercise 3/main.js
+++ Exercises/Class 4/Exercise 3/main.js
@@ -2,8 +2,15 @@ let timerDisplay = document.querySelector(".timer");
 const timeInput = document.querySelector(".time-input");
 const startButton = document.querySelector(".start");
 const cancelButton = document.querySelector(".cancel");
+
 let timeLeft = 0;
 let timerInterval;
+
+startButton.style.backgroundColor = "green";
+cancelButton.style.backgroundColor = "#A43333";
+cancelButton.style.display = "block";
+timeInput.style.display = "block";
+startButton.style.display = "block";
 function startTimer(){
     timerInterval = setInterval("timer()", 1000);
     timeLeft = timeInput.value;
@@ -27,7 +34,24 @@ function stopTimer(){
 }
 
 startButton.addEventListener("click", startTimer);
+startButton.addEventListener("mouseover", function(){
+    startButton.style.backgroundColor = "#CA9A9A";
+});
+startButton.addEventListener("mouseout", function(){
+    startButton.style.backgroundColor = "green";
+});
 cancelButton.addEventListener("click", stopTimer);
+cancelButton.addEventListener("mouseover", function(){
+    cancelButton.style.backgroundColor = "#CA9A9A";
+});
+cancelButton.addEventListener("mouseout", function(){
+    cancelButton.style.backgroundColor = "#A43333";
+});
 timeInput.addEventListener("change", function() {
     timerDisplay.innerText = timeInput.value;
 });
+console.log(timerDisplay.clientWidth);
+
+
+
+//CA9A9A - Button Pressed color
